08b79b2e0ac996173b2143fb9faf5b8823f4e7a0
Nkosenye10Developer <nkosenyehlezawht@gmail.com>
2023-07-10 11:18:17 +0200

    first commit

---
+++ config/corsOptions.js
@@ -0,0 +1,34 @@
+
+
+// const whitelist = [
+//     'https://www.yoursite.com',
+//      'http://127.0.0.1:5500' , 
+//      'http://localhost:3500'
+//  ];
+// const corsOptions = {
+//     origin: (origin,callback) => {
+//         if(whitelist.indexOf(origin) !== -1 || !origin ){
+//             callback(null, true)
+//         }else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     optionsSuccessStatus:200
+// }
+
+
+const allowedOrigins = require('./allowedOrigins');
+
+const corsOptions = { 
+    origin : (origin,callback) => {
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            callback(null,true);
+        }else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionSuccessStatus :200
+}
+
+
+module.exports = corsOptions;
